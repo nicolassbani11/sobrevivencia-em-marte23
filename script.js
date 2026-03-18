a3250eeabdeee20a3afd4c7685196b1a8e0289d8
I1HN <nicolas.bani@edu.senai.br>
2026-03-18 08:56:54 -0300

    adicionando o meu script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let oxigenio = Number(prompt("insira a quantidade de oxigenio restante ( em litr
 let pessoas = Number (prompt(" insira a quantidade de pessoas em marte"));
 const consumoDiario = 15; // litros por pessoa.
 // consumo total é baseado na quantidade de pessoas multiplicado pelo consumo diario por pessoa
-let comsumoTotalBase = pessoas * consumoDiario;
-let diasRestantes = oxigenio / comsumoTotalBase;
+let consumoTotalBase = pessoas * consumoDiario;
+let diasRestantes = oxigenio / consumoTotalBase;
 
 alert(`relatório de marte: ${diasRestantes.toFixed(0)}dias de oxigenio`);
